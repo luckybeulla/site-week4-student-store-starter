df71607418ffa93a831634fae579196276a8c1b9
Lucky Beulla Muhoza <muhozaluckyb03@gmail.com>
2024-06-27 18:21:02 -0700

    Update orderRoutes.js

--- student-store-api/routes/orderRoutes.js
+++ student-store-api/routes/orderRoutes.js
@@ -9,7 +9,7 @@ router.post("/", orderController.createOrder);
 router.put("/:id", orderController.updateOrder);
 router.delete("/:id", orderController.deleteOrder);
 
-router.post("/:order_id/items", orderController.addOrderItem);
+router.post("/:order_id", orderController.addOrderItem);
 router.get("/:order_id/total", orderController.getOrderTotalPrice);
 
 module.exports = router;
